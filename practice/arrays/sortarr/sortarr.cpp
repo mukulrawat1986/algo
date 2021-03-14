572ea3ee8280f2348c957c3535362e2a94cad95a
Mukul Rawat <mukulsrawat@gmail.com>
2021-03-14 23:13:49 +0530

    program to sort array in ascending order

---
+++ practice/arrays/sortarr/sortarr.cpp
@@ -0,0 +1,27 @@
+// Using the inbuilt sort function of c++
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int a[1000];
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+
+    // Sort an array using sort function
+    sort(a, a+n);
+
+    // print the array
+    for(int i=0; i<n; i++) cout << a[i] << "\n";
+    return 0;
+}
