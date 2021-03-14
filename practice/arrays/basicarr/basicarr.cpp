34f6ed6682303519c1dc3bc5fb14e278583248b2
Mukul Rawat <mukulsrawat@gmail.com>
2021-03-14 19:26:30 +0530

    initialize and print arrays

---
+++ practice/arrays/basicarr/basicarr.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // initialize all elements of array as 0 and print them
+    int a[10] {};
+    for(int &i: a) cout << i << "\n";
+
+    cout << "\n";
+
+    // initialize 3 elements of array and set rest to 0
+    int b[10] {1,2,3};
+    for(int &i: b) cout << i << "\n";
+
+    return 0;
+}
