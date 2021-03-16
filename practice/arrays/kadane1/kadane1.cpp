40ed9d80fb61aaad1b60cec78e70ee9acac92292
Mukul Rawat <mukulsrawat@gmail.com>
2021-03-17 01:23:55 +0530

    kadane algorithm to find min subarray

---
+++ practice/arrays/kadane1/kadane1.cpp
@@ -0,0 +1,22 @@
+// kadane's algo for minimum sum
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+    int n, a[1000];
+    cin >> n;
+    for(int i=0; i<n; i++) cin>>a[i];
+
+    int sum = 0, ans = a[0];
+    for(int i=0; i<n; i++) {
+        sum += a[i];
+        ans = min(ans, sum);
+        sum = min(sum, 0);
+    }
+    cout << ans << "\n";
+
+    return 0;
+}
